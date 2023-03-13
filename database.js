a15141707082023a11dcce6b47a1ef7a1fdb2151
Gabriel Petersson <gabriel@littleland.se>
2023-03-13 12:02:47 +0100

    added access control depending on role

--- database.js
+++ database.js
@@ -2,9 +2,13 @@ const sqlite3 = require('sqlite3').verbose();
 let sql;
 
 
-const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) return console.error(err.message);
-});
+function connect() {
+  const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+  });
+  return db;
+}
+
 
 // create table Users
 /* sql = 'CREATE TABLE Users(userID PRIMARY KEY, role, name, password)'
@@ -20,10 +24,44 @@ db.run(sql, ["admin", "admin", "admin", "admin"], (err) => {
 }); */
 
 // display everyting in Users
-sql = 'SELECT * FROM Users';
+/* sql = 'SELECT * FROM Users';
 db.all(sql, [], (err, rows) => {
   if (err) return console.error(err.message);
   rows.forEach(row => {
     console.log(row);
   })
-})
+}) */
+
+
+/* const addUser = (username, password) => {
+  var db = connect();
+  sql = 'INSERT INTO users(username, password) VALUES (?,?)';
+  db.run(
+    sql,
+    [username, password],
+    (err) => {
+      if (err) return console.error(err.message);
+    }
+  )
+
+  var sql2 = 'SELECT * from users';
+  db.all(sql2, [], (err, rows) => {
+    if (err) return reject(err);
+    console.log(rows);
+  });
+} */
+
+const getName = (name) => {
+  var db = connect();
+  return new Promise((resolve, reject) => {
+    var sql = 'SELECT * from Users WHERE name = (?)';
+    db.all(sql, [name], (err, rows) => {
+      if (err) return reject(err);
+      resolve(rows);
+    });
+  })
+}
+
+module.exports = {
+  getName,
+}
--- server.js
+++ server.js
@@ -3,19 +3,119 @@ const app = express()
 const jwt = require('jsonwebtoken')
 require("dotenv").config()
 const bcrypt = require('bcrypt')
-
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+const database = require('./database');
 
 var currentKey = ""
 var currentPassoword = ""
+var role
+
+app.listen(8000)
 
 app.get('/', (req, res) => {
-  res.redirect("/identify")
+  res.redirect("/LOGIN")
 })
 
-app.post('/identify', (req, res) => {
+app.get('/LOGIN', (req, res) => {
+  res.render('Login.ejs')
+})
+
+app.post('/LOGIN', async (req, res) => {
+  let name = req.body.name;
+  let password = req.body.password;
+  try {
+    const rows = await database.getName(name);
+    if (typeof rows !== 'undefined' && rows.length === 0) {
+      console.log("Not an existing user");
+      res.sendStatus(401);
+      /* res.render('fail.ejs'); */
+    } else {
+      console.log("This is an existing user");
+      try {
+        // since the user in the databse are manually implemented...
+        /* const passwordIsValid = await bcrypt.compare(password, rows[0].password); */
+        if (password === rows[0].password) {
+          console.log("The password is valid");
+          // create token
+          var token = jwt.sign(name, process.env.ACCESS_TOKEN_SECRET)
+          currentKey = token
+          currentPassoword = password
+          role = rows[0].role
+          res.redirect("/granted")
+        } else {
+          res.sendStatus(401)
+          console.log("The password is not valid");
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).send();
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
+})
+
+function authenticateToken(req, res, next) {
+  if (currentKey == "") {
+    res.redirect("/LOGIN")
+  } else if (jwt.verify(currentKey, process.env.ACCESS_TOKEN_SECRET)) {
+    next();
+  } else {
+    res.redirect("/LOGIN")
+  }
+}
+
+app.get('/granted', authenticateToken, (req, res) => {
+  res.render('start.ejs')
+})
+
+app.get('/admin', authenticateToken, (req, res) => {
+  if (role === "admin") {
+    res.render('admin.ejs')
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+
+
+
+app.post('/REGISTER', async (req, res) => {
+  console.log(req.body.username)
+  console.log(req.body.password)
+
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    console.log(hashedPassword)
+    let username = req.body.username
+    database.addUser(username, hashedPassword)
+    res.redirect('/LOGIN')
+    res.status(201).send()
+
+  } catch {
+    console.log("dident work...")
+    res.status(500).send()
+  }
+
+})
+
+app.get('/REGISTER', (req, res) => {
+  res.render('register.ejs')
+})
+
+
+
+
+
+
+
+
+
+/* app.post('/identify', (req, res) => {
   // authentication
   const username = req.body.password
   const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
@@ -43,4 +143,7 @@ app.get('/granted', authenticateToken, (req, res) => {
   res.render("start.ejs")
 })
 
-app.listen(8000)
+app.get('/admin', (req, res) => {
+  res.render('admin.ejs')
+}) */
+
